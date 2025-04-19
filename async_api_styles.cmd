4debd83c7a79efaaedf57ac3242f7f400ca7a926
grishavanika <grish.vanika@gmail.com>
2025-04-19 18:35:55 +0300

    cpp

--- async_api_styles.cmd
+++
@@ -1,9 +0,0 @@
-call pandoc -s --toc --toc-depth=4 ^
-  --standalone ^
-  --number-sections ^
-  --highlight=kate ^
-  --from markdown --to=html5 ^
-  --lua-filter=anchor-links.lua ^
-  async_api_styles.md ^
-  -o async_api_styles.html
-
